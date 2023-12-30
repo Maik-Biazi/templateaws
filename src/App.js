bb48e20c3d4b567ee87cde34cdb5f660076734a5
Maik-Biazi <71233006+Maik-Biazi@users.noreply.github.com>
2023-12-29 22:40:46 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   const [ip, setIp] = useState("");
   const [region, setRegion] = useState("");
   const [zone, setZone] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     // Obter o IP público da instância EC2
@@ -38,12 +40,44 @@ function App() {
       });
   }, []);
 
+  //Função para lidar com a mudança no input de nome
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  // Função para lidar com a mudança no input de e-mail
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  // Função para lidar com o clique no botão de salvar
+  const handleSaveClick = () => {
+    // Salvar nome e e-mail no localStorage
+    localStorage.setItem("name", name);
+    localStorage.setItem("email", email);
+  };
+
   return (
     <div>
       <h1>Metadados da instância EC2</h1>
       <p>IP público: {ip}</p>
       <p>Região: {region}</p>
       <p>Zona de disponibilidade: {zone}</p>
+  
+
+      {/* Adicionar inputs para nome e e-mail */}
+      <label>
+        Nome:
+        <input type="text" value={name} onChange={handleNameChange} />
+      </label>
+      <br />
+      <label>
+        E-mail:
+        <input type="text" value={email} onChange={handleEmailChange} />
+      </label>
+      <br />
+      {/* Adicionar botão para salvar no localStorage */}
+      <button onClick={handleSaveClick}>Salvar no LocalStorage</button>
     </div>
   );
 }
