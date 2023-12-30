c92962b750074598b25cf27f68a911e8c49bb132
Maik-Biazi <71233006+Maik-Biazi@users.noreply.github.com>
2023-12-29 22:34:44 -0300

    primeiro comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [ip, setIp] = useState("");
+  const [region, setRegion] = useState("");
+  const [zone, setZone] = useState("");
+
+  useEffect(() => {
+    // Obter o IP público da instância EC2
+    axios
+      .get("http://169.254.169.254/latest/meta-data/public-ipv4")
+      .then((response) => {
+        setIp(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    // Obter a região da instância EC2
+    axios
+      .get("http://169.254.169.254/latest/meta-data/placement/region")
+      .then((response) => {
+        setRegion(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    // Obter a zona de disponibilidade da instância EC2
+    axios
+      .get("http://169.254.169.254/latest/meta-data/placement/availability-zone")
+      .then((response) => {
+        setZone(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Metadados da instância EC2</h1>
+      <p>IP público: {ip}</p>
+      <p>Região: {region}</p>
+      <p>Zona de disponibilidade: {zone}</p>
     </div>
   );
 }
